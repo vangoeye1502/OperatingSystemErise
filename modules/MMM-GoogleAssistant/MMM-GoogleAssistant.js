99ad27dba333672328dd9803342a929d768dc8d0
jannesvangoeye <jannes.van.goeye@gmail.com>
2017-12-02 23:14:15 +0100

    Update Google Assistant Module

--- modules/MMM-GoogleAssistant/MMM-GoogleAssistant.js
+++ modules/MMM-GoogleAssistant/MMM-GoogleAssistant.js
@@ -9,7 +9,7 @@ Module.register("MMM-GoogleAssistant", {
   // Module config defaults.
   defaults: {
 
-    header: "Google Asistant",
+    header: "Google Assistant",
     maxWidth: "100%",
     publishKey: 'pub-c-b7f324ac-e190-4bcf-ba5e-b860168f6f9b',
     subscribeKey: 'sub-c-f97a2b14-b03f-11e7-8d4b-66b981d3b880',
@@ -30,18 +30,19 @@ Module.register("MMM-GoogleAssistant", {
 
     if (this.assistantActive == true) {
       if (this.processing == true) {
-        wrapper.innerHTML = "<img src='MMM-GoogleAssistant/assistant_active.png'></img><br/>" + this.userQuery.toUpperCase() + "?";
+          wrapper.innerHTML = "<img src='MMM-GoogleAssistant/assistant_active.png'></img><br/>" + this.userQuery.toUpperCase() + "?";
       } else {
-        wrapper.innerHTML = "<div class=\"sk-folding-cube\">\n" +
+        /*wrapper.innerHTML = "<div class=\"sk-folding-cube\">\n" +
             "  <div class=\"sk-cube1 sk-cube\"></div>\n" +
             "  <div class=\"sk-cube2 sk-cube\"></div>\n" +
             "  <div class=\"sk-cube4 sk-cube\"></div>\n" +
             "  <div class=\"sk-cube3 sk-cube\"></div>\n" +
-            "</div><br/>" + "LISTENING";
+            "</div><br/>" + "LISTENING";*/
+          wrapper.innerHTML = "What can I help you with?";
       }
 
     } else {
-      wrapper.innerHTML = "Say, \"Hey Google\" ";
+        wrapper.innerHTML = "Say, \"Hey Google\" ";
     }
     return wrapper;
   },
